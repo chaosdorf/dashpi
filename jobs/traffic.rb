f34d3326aaf9a17b1d28047e5bd6b7986ad4813a
nomaster <nomaster@chaosdorf.de>
2013-04-09 15:49:56 +0200

    Traffic: use moving median from last 5 minutes

--- jobs/traffic.rb
+++ jobs/traffic.rb
@@ -1,17 +1,17 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '10s', :first_in => 0 do |job|
-  json = Net::HTTP.get('graphserver', '/render/?target=scale(summarize(derivative(servers.dn42.chaosdorf.figurehead.network.if_inet.up),"10minutes"),0.1))&from=-10minutes&format=json')
-  value = JSON.parse(json)[0]['datapoints'][0][0]
+SCHEDULER.every '5m', :first_in => 0 do |job|
+  json = Net::HTTP.get('graphserver', '/render/?target=movingMedian(derivative(keepLastValue(servers.dn42.chaosdorf.figurehead.network.if_inet.up)),5)&from=-5minutes&format=json')
+  value = JSON.parse(json)[0]['datapoints'].last[0]
   if value
-    send_event('uplink-traffic', { value: value })
+    send_event('uplink-traffic', { value: value.to_i })
     puts "Uplink Traffic: #{value}"
   else
     puts "Uplink Traffic: #{value}"
   end
-  json = Net::HTTP.get('graphserver', '/render/?target=scale(summarize(derivative(servers.dn42.chaosdorf.figurehead.network.if_inet.down),"10minutes"),0.1))&from=-10minutes&format=json')
-  value = JSON.parse(json)[0]['datapoints'][0][0]
+  json = Net::HTTP.get('graphserver', '/render/?target=movingMedian(derivative(keepLastValue(servers.dn42.chaosdorf.figurehead.network.if_inet.down)),5)&from=-5minutes&format=json')
+  value = JSON.parse(json)[0]['datapoints'].last[0]
   if value
-    send_event('downlink-traffic', { value: value })
+    send_event('downlink-traffic', { value: value.to_i })
     puts "Downlink Traffic: #{value}"
   else
     puts "Downlink Traffic: (error)"
