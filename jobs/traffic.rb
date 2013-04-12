3b34a1d4f6692bdc12def72cd0b897768e91dc8b
nomaster <nomaster@chaosdorf.de>
2013-04-13 00:43:35 +0200

    Traffic: now with correct scaling, I guess

--- jobs/traffic.rb
+++ jobs/traffic.rb
@@ -1,6 +1,6 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '5m', :first_in => 0 do |job|
-  json = Net::HTTP.get('graphserver', '/render/?target=movingMedian(derivative(keepLastValue(servers.dn42.chaosdorf.figurehead.network.if_inet.up)),5)&from=-5minutes&format=json')
+  json = Net::HTTP.get('graphserver', '/render/?target=movingMedian(derivative(keepLastValue(scale(servers.dn42.chaosdorf.figurehead.network.if_inet.up,0.08))),5)&from=-5minutes&format=json')
   value = JSON.parse(json)[0]['datapoints'].last[0]
   if value
     send_event('uplink-traffic', { value: value.to_i })
@@ -8,7 +8,7 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
   else
     puts "Uplink Traffic: #{value}"
   end
-  json = Net::HTTP.get('graphserver', '/render/?target=movingMedian(derivative(keepLastValue(servers.dn42.chaosdorf.figurehead.network.if_inet.down)),5)&from=-5minutes&format=json')
+  json = Net::HTTP.get('graphserver', '/render/?target=movingMedian(derivative(keepLastValue(scale(servers.dn42.chaosdorf.figurehead.network.if_inet.down,0.08))),5)&from=-5minutes&format=json')
   value = JSON.parse(json)[0]['datapoints'].last[0]
   if value
     send_event('downlink-traffic', { value: value.to_i })
