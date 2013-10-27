9bb64b226c7c8d8b74833a0a077f9cdbffb96a8d
nomaster <nomaster@chaosdorf.de>
2013-10-27 03:22:42 +0100

    on change updated MPD widget

--- jobs/mpd.rb
+++ jobs/mpd.rb
@@ -1,10 +1,26 @@
 require 'ruby-mpd'
-SCHEDULER.every '2s', :first_in => 0 do
-  mpd = MPD.new 'mpd.chaosdorf.dn42', 6600
-  mpd.connect
-  song = mpd.current_song
-  text = "#{song.artist}<hr />#{song.title}"
+
+def update_dashboard(mpd)
+  if mpd.current_song
+    artist = "#{mpd.current_song.artist}"
+    title = "#{mpd.current_song.title}"
+    text = [artist, title].join(" – ")
+  else
+    text = "[empty playlist]"
+  end
+  text += "<br><small>Volume: #{mpd.volume}%</small><br>"
   send_event('mpd-status', { text: text })
-  mpd.disconnect
+end
+
+SCHEDULER.in '0s' do
+  mpd = MPD.new 'mpd.chaosdorf.dn42', 6600, {callbacks: true}
+  mpd.connect
+  update_dashboard(mpd)
+  mpd.on :volume do |volume|
+    update_dashboard(mpd)
+  end
+  mpd.on :song do |song|
+    update_dashboard(mpd)
+  end
 end
 
