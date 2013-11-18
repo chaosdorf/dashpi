cc248151529404cfda9aa8d062d432599ec58715
nomaster <nomaster@chaosdorf.de>
2013-11-18 01:43:42 +0100

    get traffic from realtime collectd data

--- jobs/traffic.rb
+++ jobs/traffic.rb
@@ -1,13 +1,8 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '5m', :first_in => 0 do |job|
-  json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render/?target=movingMedian(derivative(keepLastValue(scale(servers.dn42.chaosdorf.figurehead.network.if_inet.up,0.08))),5)&from=-5minutes&format=json')
-  value = JSON.parse(json)[0]['datapoints'].last[0]
-  if value
-    send_event('uplink-traffic', { value: value.to_i })
-  end
-  json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render/?target=movingMedian(derivative(keepLastValue(scale(servers.dn42.chaosdorf.figurehead.network.if_inet.down,0.08))),5)&from=-5minutes&format=json')
-  value = JSON.parse(json)[0]['datapoints'].last[0]
-  if value
-    send_event('downlink-traffic', { value: value.to_i })
-  end
+SCHEDULER.every '1s', :first_in => 0 do |job|
+  json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render/?target=scale(keepLastValue(collectd.figurehead_chaosdorf_dn42.interface-inet.if_octets.*),8)&format=json&from=-60seconds')
+  rx = JSON.parse(json)[0]['datapoints'].last()[0]
+  tx = JSON.parse(json)[1]['datapoints'].last()[0]
+  send_event('downlink-traffic', { value: rx })
+  send_event('uplink-traffic', { value: tx })
 end
