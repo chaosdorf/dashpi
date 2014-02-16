0793e0ccf5bdb248e4982bc53abe629aa3dbd561
Micoto Szillat <mic@fluxent.de>
2014-02-17 00:22:29 +0100

    Freifunk node counter repaired

--- jobs/freifunk.rb
+++ jobs/freifunk.rb
@@ -1,8 +1,8 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render/?target=keepLastValue(servers.dn42.chaosdorf.figurehead.network.freifunk_nodes.total)&from=-24hours&until&format=json')
+  json = Net::HTTP.get('collectd.rheinufer.freifunk-rheinland.net', '/render/?target=maxSeries(keepLastValue(collectd.rheinufer*.batman.gauge-originators))&from=-1minute&format=json')
   values = JSON.parse(json)[0]['datapoints']
-  points = values.collect { |value|
+    points = values.collect { |value|
     y = value[0]
     x = value[1]
     { :x => y, :y => y }
