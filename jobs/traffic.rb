7c801d2fd7ccbceb29456ca06813be4d2619dee6
Micoto Szillat <mic@fluxent.de>
2014-09-08 01:19:12 +0200

    pull traffic from new router

--- jobs/traffic.rb
+++ jobs/traffic.rb
@@ -1,6 +1,6 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1s', :first_in => 0 do |job|
-  json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render/?target=scale(keepLastValue(collectd.figurehead_chaosdorf_dn42.interface-inet.if_octets.*),8)&format=json&from=-60seconds')
+  json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render/?target=scale(keepLastValue(collectd.selene_chaosdorf_dn42.snmp.if_octets-bond0_2.*),8)&format=json&from=-10seconds')
   rx = JSON.parse(json)[0]['datapoints'].last()[0]
   tx = JSON.parse(json)[1]['datapoints'].last()[0]
   send_event('downlink-traffic', { value: rx })
