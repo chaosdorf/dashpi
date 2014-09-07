2baef08fc68b1f091f0d46d57d011e288c79356f
Micoto Szillat <mic@fluxent.de>
2014-09-08 01:51:53 +0200

    adjusted traffic query variables

--- jobs/traffic.rb
+++ jobs/traffic.rb
@@ -1,6 +1,6 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '1s', :first_in => 0 do |job|
-  json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render/?target=scale(keepLastValue(collectd.selene_chaosdorf_dn42.snmp.if_octets-bond0_2.*),8)&format=json&from=-10seconds')
+SCHEDULER.every '5s', :first_in => 0 do |job|
+  json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render/?target=scale(keepLastValue(collectd.selene_chaosdorf_dn42.snmp.if_octets-bond0_2.*),8)&format=json&from=-60seconds')
   rx = JSON.parse(json)[0]['datapoints'].last()[0]
   tx = JSON.parse(json)[1]['datapoints'].last()[0]
   send_event('downlink-traffic', { value: rx })
