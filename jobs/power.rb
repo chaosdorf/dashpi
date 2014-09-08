14088689107c81132c320a25be3b0e7b6d30c309
Micoto Szillat <mic@fluxent.de>
2014-09-08 02:29:12 +0200

    pull power metric directly from flukso

--- jobs/power.rb
+++ jobs/power.rb
@@ -1,7 +1,11 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '1m', :first_in => 0 do |job|
-  json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render?from=-10minutes&target=sum(keepLastValue(collectd.figurehead_chaosdorf_dn42.tail-power.gauge-phase_*))&format=json')
-  value = JSON.parse(json)[0]['datapoints'].last()[0].to_i
+SCHEDULER.every '1s', :first_in => 0 do |job|
+  sensors = ['d80587d41bebde066f003a8f60ac0d01','2267a0503927a5f2bbf0050f657dcc55','90d083c153310b5787e3f1a7fc7967a5']
+  value = 0
+  (0..2).each do |i|
+    json = Net::HTTP.get('flukso.chaosdorf.dn42',"/sensor/#{sensors[i]}?version=1.0&interval=minute&unit=watt&callback=realtime",'8080')
+    value += JSON.parse(json)[-2][1].to_i
+  end
   send_event('power-total', { current: value })
   puts value
 end
