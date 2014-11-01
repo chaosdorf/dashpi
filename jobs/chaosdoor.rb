33f487e397053336c26c4b754b1b17a9b7500e1f
Mic <nomaster@nomaster.cc>
2014-11-02 00:18:52 +0100

    door warnings

--- jobs/chaosdoor.rb
+++ jobs/chaosdoor.rb
@@ -1,4 +1,12 @@
+last = "unknown"
 SCHEDULER.every '10s', :first_in => 0 do
-  status = Net::HTTP.get('door.chaosdorf.dn42','/status')
-  send_event('chaosdoor-mode', { text: status } )
+  value = Net::HTTP.get('door.chaosdorf.dn42','/status')
+  status = "normal"
+  if value != last
+    status = "warning"
+  elsif value = "open\n"
+    status = "danger"
+  end
+  send_event('chaosdoor-mode', { text: value, status: status} )
+  last = value
 end
