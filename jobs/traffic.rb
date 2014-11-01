eced7d786961729f80c7ffde7a982896db5a405d
Mic <nomaster@nomaster.cc>
2014-11-01 17:42:50 +0100

    traffic warnings

--- jobs/traffic.rb
+++ jobs/traffic.rb
@@ -1,8 +1,26 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '5s', :first_in => 0 do |job|
   json = Net::HTTP.get('graphserver.chaosdorf.dn42', '/render/?target=scale(keepLastValue(collectd.selene_chaosdorf_dn42.snmp.if_octets-bond0_2.*),8)&format=json&from=-60seconds')
-  rx = JSON.parse(json)[0]['datapoints'].last()[0]
-  tx = JSON.parse(json)[1]['datapoints'].last()[0]
-  send_event('downlink-traffic', { value: rx })
-  send_event('uplink-traffic', { value: tx })
+  rx = Hash.new
+  tx = Hash.new
+  rx['value'] = JSON.parse(json)[0]['datapoints'].last()[0]
+  tx['value'] = JSON.parse(json)[1]['datapoints'].last()[0]
+  case tx['value']
+  when 0
+    tx['status'] = "danger"
+  when 0..5000000
+    tx['status'] = "normal"
+  else
+    tx['status'] = "warning"
+  end
+  case rx['value']
+  when 0
+    rx['status'] = "danger"
+  when 0..100000000
+    rx['status'] = "normal"
+  else
+    rx['status'] = "warning"
+  end
+  send_event('downlink-traffic', { value: rx['value'], status: rx['status'] })
+  send_event('uplink-traffic', { value: tx['value'], status: tx['status'] })
 end
--- widgets/meter/meter.coffee
+++ widgets/meter/meter.coffee
@@ -12,3 +12,11 @@ class Dashing.Meter extends Dashing.Widget
     meter.attr("data-bgcolor", meter.css("background-color"))
     meter.attr("data-fgcolor", meter.css("color"))
     meter.knob()
+
+  onData: (data) ->
+    if data.status
+      # clear existing "status-*" classes
+        $(@get('node')).attr 'class', (i,c) ->
+          c.replace /\bstatus-\S+/g, ''
+        # add new class
+        $(@get('node')).addClass "status-#{data.status}"
