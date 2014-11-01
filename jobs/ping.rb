98d17febf435e705dd0539dc9f2d95cb61082c9b
Mic <nomaster@nomaster.cc>
2014-11-01 23:21:58 +0100

    added ping warning, increased widget size

--- jobs/ping.rb
+++ jobs/ping.rb
@@ -2,11 +2,22 @@
 require 'net/ping/tcp'
 
 host = "80.69.100.220" # speedtest.unitymedia.de
+last = 0
 
 SCHEDULER.every '5s', :first_in => 0 do |job|
   probe = Net::Ping::TCP.new(host)
   probe.port = 80
   probe.ping
   value = (probe.duration * 1000).round()
-  send_event('ping', { current: value })
+  status = "normal"
+  case value
+  when 0..100
+    status = "normal"
+  when 100..200
+    status = "danger"
+  else
+    status = "warning"
+  end
+  send_event('ping', { current: value, last: last, status: status })
+  last = value
 end
