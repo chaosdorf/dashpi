292b62d6b15a4ce6334f9fd542754b01dccbcf7d
Mic <nomaster@nomaster.cc>
2014-11-01 23:09:21 +0100

    power and traffic warnings

--- jobs/power.rb
+++ jobs/power.rb
@@ -2,9 +2,19 @@
 SCHEDULER.every '1s', :first_in => 0 do |job|
   sensors = ['d80587d41bebde066f003a8f60ac0d01','2267a0503927a5f2bbf0050f657dcc55','90d083c153310b5787e3f1a7fc7967a5']
   value = 0
+  last = 0
   (0..2).each do |i|
     json = Net::HTTP.get('flukso.chaosdorf.dn42',"/sensor/#{sensors[i]}?version=1.0&interval=minute&unit=watt&callback=realtime",'8080')
     value += JSON.parse(json)[-2][1].to_i
+    last += JSON.parse(json)[-7][1].to_i
   end
-  send_event('power-total', { current: value })
+  case value
+  when 0..1500
+    status = "normal"
+  when 1500..3000
+    status = "danger"
+  else
+    status = "warning"
+  end
+  send_event('power-total', { current: value, last: last, status: status })
 end
--- jobs/traffic.rb
+++ jobs/traffic.rb
@@ -6,18 +6,18 @@ SCHEDULER.every '5s', :first_in => 0 do |job|
   rx['value'] = JSON.parse(json)[0]['datapoints'].last()[0]
   tx['value'] = JSON.parse(json)[1]['datapoints'].last()[0]
   case tx['value']
-  when 0
-    tx['status'] = "danger"
-  when 0..5000000
+  when 0..3000000
     tx['status'] = "normal"
+  when 3000000..4000000
+    tx['status'] = "danger"
   else
     tx['status'] = "warning"
   end
   case rx['value']
-  when 0
-    rx['status'] = "danger"
-  when 0..100000000
+  when 0..60000000
     rx['status'] = "normal"
+  when 60000000..90000000
+    rx['status'] = "danger"
   else
     rx['status'] = "warning"
   end
