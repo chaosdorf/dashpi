17ac50fe7bc8dd2ce67f7b144f468d8e10bfe369
Mic <nomaster@nomaster.cc>
2015-01-17 20:20:25 +0100

    Freifunk counter repaired

--- jobs/freifunk.rb
+++ jobs/freifunk.rb
@@ -1,6 +1,6 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  json = Net::HTTP.get('collectd.freifunk-rheinland.net', '/render/?target=maxSeries(keepLastValue(collectd.rheinufer*.batman.gauge-nodes))&from=-1minute&format=json')
-  value = JSON.parse(json)[0]['datapoints'][0][0]
+  json = Net::HTTP.get('ffmap.freifunk-rheinland.net', '/alfred_merged.json')
+  value = json.split("\n").grep(/node_id/).length()
   send_event('freifunk-total', { current: value })
 end
