a1d3ff47007776934c24a4e15691846e7674012a
Mic <nomaster@nomaster.cc>
2015-09-27 21:25:41 +0200

    Freifunk Rheinufer to Düsseldorf

--- jobs/freifunk.rb
+++ jobs/freifunk.rb
@@ -1,6 +1,6 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  json = Net::HTTP.get('ffmap.freifunk-rheinland.net', '/alfred_merged.json')
+  json = Net::HTTP.get('map.freifunk-duesseldorf.de', '/alfred_merged.json')
   value = json.split("\n").grep(/node_id/).length()
   send_event('freifunk-total', { current: value })
 end
