4e3e9b77a45b0602e125607f253314bdee8b1cbb
Niklas Sombert <niklas@ytvwld.de>
2016-04-01 21:39:46 +0200

    twitter: use streaming

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -3,17 +3,36 @@ require 'dotenv'
 
 Dotenv.load
 
+count_tweets = 10
+
+tweets = Array.new(count_tweets).fill({name: '', body: '', avatar: ''})
+
+def add_tweet(tweets, tweet)
+  tweets.rotate!
+  tweets[0] = {name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url}
+end
+
+streaming_client = Twitter::Streaming::Client.new do |config|
+  ['consumer_key', 'consumer_secret', 'access_token', 'access_token_secret'].each do |c|
+    config.send( "#{c}=", ENV["TWITTER_#{c.upcase}"] )
+  end
+end
+
 rest_client = Twitter::REST::Client.new do |config|
   ['consumer_key', 'consumer_secret', 'access_token', 'access_token_secret'].each do |c|
     config.send( "#{c}=", ENV["TWITTER_#{c.upcase}"] )
   end
 end
 
-SCHEDULER.every '60s' do
+SCHEDULER.every '5m', :first_in => 0 do |job|
   mentions = rest_client.mentions_timeline
-  tweets = []
-  mentions.take(5).each do |t|
-    tweets << {name: t.user.name, body: t.text, avatar: t.user.profile_image_url}
+  mentions.take(count_tweets).each do |tweet|
+    add_tweet(tweets, tweet)
   end
+  
   send_event 'twitter_mentions', comments: tweets
+  streaming_client.filter(track: 'chaosdorf') do |tweet|
+    add_tweet(tweets, tweet)
+    send_event 'twitter_mentions', comments: tweets
+  end
 end
