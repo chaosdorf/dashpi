c253e545b86ab25bb29d8c757e6679dbfe468098
Niklas <niklas@ytvwld.de>
2016-04-03 20:31:10 +0200

    weather: Net::HTTP.get

--- jobs/weather.rb
+++ jobs/weather.rb
@@ -12,9 +12,8 @@ woe_id = 646099
 format = 'c'
 
 SCHEDULER.every '15m', :first_in => 0 do |job|
-  http = Net::HTTP.new('xml.weather.yahoo.com') #via https://stackoverflow.com/a/36262971
-  response = http.request(Net::HTTP::Get.new("/forecastrss?w=#{woe_id}&u=#{format}"))
-  weather_data = XmlSimple.xml_in(response.body, { 'ForceArray' => false })['channel']['item']['condition']
-  weather_location = XmlSimple.xml_in(response.body, { 'ForceArray' => false })['channel']['location']
+  response = Net::HTTP.get('xml.weather.yahoo.com', "/forecastrss?w=#{woe_id}&u=#{format}") #via https://stackoverflow.com/a/36262971
+  weather_data = XmlSimple.xml_in(response, { 'ForceArray' => false })['channel']['item']['condition']
+  weather_location = XmlSimple.xml_in(response, { 'ForceArray' => false })['channel']['location']
   send_event('weather', { :temp => "#{weather_data['temp']}&deg;#{format.upcase}", :condition => weather_data['text'], :title => "#{weather_location['city']} Weather"})
 end
