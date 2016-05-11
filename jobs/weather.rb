3b9b7c6923825a0a868c2d707d916c0e1ea91e2b
Niklas Sombert <niklas@ytvwld.de>
2016-05-11 09:27:43 +0200

    weather: display time and date more nicely

--- jobs/weather.rb
+++ jobs/weather.rb
@@ -2,14 +2,16 @@
 
 require 'net/http'
 require 'xmlsimple'
+require 'time'
+require 'date'
 
 SCHEDULER.every '1h', :first_in => 0 do |job|
   response = Net::HTTP.get("www.yr.no", "/place/Germany/North_Rhine-Westphalia/Düsseldorf/forecast.xml")
   xml = XmlSimple.xml_in(response)
   location = xml["location"][0]["name"][0]
   weather_data = []
-  xml["forecast"][0]["tabular"][0]["time"][0...9].each do |data| #today and two days after
-    weather_data << {time: "#{data["from"]} - #{data["to"]}",
+  xml["forecast"][0]["tabular"][0]["time"][0...9].each_with_index do |data, index| #today and two days after
+    weather_data << {time: format_time(data["from"], data["to"], index),
 		 condition: data["symbol"][0]["name"],
 		 symbol: climacon_class(data["symbol"][0]["name"]),
 		 temp: "#{data["temperature"][0]["value"]}°C"}
@@ -18,6 +20,46 @@ SCHEDULER.every '1h', :first_in => 0 do |job|
                           :weather_data => weather_data})
 end
 
+def format_time(from, to, index)
+	#special case
+	if index == 0
+		return "jetzt"
+	end
+	#parsing and vars
+	ft = Time.parse(from)
+	fd = Date.parse(from)
+	tt = Time.parse(to)
+	td = Date.parse(to)
+	day = ""
+	timeofday = ""
+	#timeofday
+	if ft.hour == 0 and tt.hour == 6
+		timeofday = "Morgen"
+	end
+	if ft.hour == 6 and tt.hour == 12
+		timeofday = "Vormittag"
+	end
+	if ft.hour == 12 and tt.hour == 18
+		timeofday = "Nachmittag"
+	end
+	if ft.hour == 18 and tt.hour == 00
+		timeofday = "Abend"
+	end
+	#day
+	#ignoring to
+	d = fd - Date.today
+	case d
+	when 0
+		day = "Heute"
+	when 1
+		day = "Morgen"
+	when 2
+		day = "Übermorgen"
+	else
+		day = fd
+	end
+	return "#{day} #{timeofday}"
+end
 
 def climacon_class(weather_code)
   #see https://github.com/christiannaths/Climacons-Font/blob/master/webfont/climacons-font.css
