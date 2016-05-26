a102242dec0901aee3992cd5ad0a3e73d276536b
Niklas Sombert <niklas@ytvwld.de>
2016-05-26 19:26:58 +0200

    weather: show only +0h to +2h

--- jobs/weather.rb
+++ jobs/weather.rb
@@ -6,60 +6,24 @@ require 'time'
 require 'date'
 
 SCHEDULER.every '1h', :first_in => 0 do |job|
-  response = Net::HTTP.get("www.yr.no", "/place/Germany/North_Rhine-Westphalia/Düsseldorf/forecast.xml")
+  response = Net::HTTP.get("www.yr.no", "/place/Germany/North_Rhine-Westphalia/Düsseldorf/forecast_hour_by_hour.xml")
   xml = XmlSimple.xml_in(response)
   location = xml["location"][0]["name"][0]
 	sun = xml["sun"][0]
-  weather_data = []
-  xml["forecast"][0]["tabular"][0]["time"][0...9].each_with_index do |data, index| #today and two days after
-    weather_data << {time: format_time(data["from"], data["to"], index),
-		 condition: data["symbol"][0]["name"],
-		 symbol: climacon_class(data["symbol"][0]["name"], is_sunlight(data["from"], data["to"], sun)),
-		 temp: "#{data["temperature"][0]["value"]}°C"}
-  end
-  send_event('weather', { :title => "Wetter für #{location}",
-                          :weather_data => weather_data})
-end
-
-def format_time(from, to, index)
-	#special case
-	if index == 0
-		return "jetzt"
-	end
-	#parsing and vars
-	ft = Time.parse(from)
-	fd = Date.parse(from)
-	tt = Time.parse(to)
-	td = Date.parse(to)
-	day = ""
-	timeofday = ""
-	#timeofday
-	if ft.hour == 0 and tt.hour == 6
-		timeofday = "früh"
-	end
-	if ft.hour == 6 and tt.hour == 12
-		timeofday = "Vormittag"
-	end
-	if ft.hour == 12 and tt.hour == 18
-		timeofday = "Nachmittag"
-	end
-	if ft.hour == 18 and tt.hour == 00
-		timeofday = "Abend"
-	end
-	#day
-	#ignoring to
-	d = fd - Date.today
-	case d
-	when 0
-		day = "Heute"
-	when 1
-		day = "Morgen"
-	when 2
-		day = "Übermorgen"
-	else
-		day = fd
-	end
-	return "#{day} #{timeofday}"
+	data = xml["forecast"][0]["tabular"][0]["time"]
+	now = {condition: data[0]["symbol"][0]["name"],
+	 symbol: climacon_class(data[0]["symbol"][0]["name"], is_sunlight(data[0]["from"], data[0]["to"], sun)),
+	 temp: "#{data[0]["temperature"][0]["value"]}°C"}
+  onehour = {condition: data[1]["symbol"][0]["name"],
+	 symbol: climacon_class(data[1]["symbol"][0]["name"], is_sunlight(data[1]["from"], data[1]["to"], sun)),
+	 temp: "#{data[1]["temperature"][0]["value"]}°C"}
+	twohour = {condition: data[2]["symbol"][0]["name"],
+ 	 symbol: climacon_class(data[2]["symbol"][0]["name"], is_sunlight(data[2]["from"], data[2]["to"], sun)),
+ 	 temp: "#{data[1]["temperature"][0]["value"]}°C"}
+  send_event('weather', { :title => "Weather",
+                          :now => now,
+													:onehour => onehour,
+													:twohour => twohour})
 end
 
 def is_sunlight(from, to, sun)
--- widgets/weather/weather.coffee
+++ widgets/weather/weather.coffee
@@ -1,26 +1,6 @@
 class Dashing.Weather extends Dashing.Widget
 
-  ready: ->
-    # This is fired when the widget is done being rendered
-    @currentIndex = 0
-    @weatherElem = $(@node).find(".weather-container")
-    @nextWeather()
-    @startCarousel()
-
   onData: (data) ->
-    @currentIndex = 0
-
-  startCarousel: ->
-    setInterval(@nextWeather, 8000)
-
-  nextWeather: =>
-    weather = @get("weather_data")
-    if weather
-      @weatherElem.fadeOut =>
-        @currentIndex = (@currentIndex + 1) % weather.length
-        current_weather = weather[@currentIndex]
-        @set "current_weather", current_weather
-        if current_weather.symbol
-          # reset classes
-          $('i.climacon').attr 'class', "climacon icon-background #{current_weather.symbol}"
-        @weatherElem.fadeIn()
+    $('#climacon-background').attr 'class', "climacon icon-background #{data.now.symbol}"
+    $('#climacon-onehour').attr 'class', "climacon #{data.onehour.symbol}"
+    $('#climacon-twohour').attr 'class', "climacon #{data.twohour.symbol}"
