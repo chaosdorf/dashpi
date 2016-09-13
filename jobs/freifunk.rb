ad817643eb980dda9da714f930747608b5f6107d
Niklas Sombert <niklas@ytvwld.de>
2016-09-13 17:16:40 +0200

    Freifunk: Handle the case if there's no connection to the internet.

--- jobs/freifunk.rb
+++ jobs/freifunk.rb
@@ -8,7 +8,11 @@ request = Net::HTTP::Get.new(uri.request_uri)
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  json = http.request(request).body
-  value = json.split("\n").grep(/node_id/).length()
-  send_event('freifunk-total', { current: value })
+  begin
+    json = http.request(request).body
+    value = json.split("\n").grep(/node_id/).length()
+    send_event('freifunk-total', { current: value, status: "normal" })
+  rescue
+    send_event('freifunk-total', { current: 0, status: "warning" })
+  end
 end
