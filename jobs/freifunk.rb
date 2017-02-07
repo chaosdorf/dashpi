9dc127ca032ff40dce8a5999db7ffa8ec3b36a30
Niklas Sombert <niklas@ytvwld.de>
2017-02-07 13:17:26 +0100

    Freifunk: new URL and new data format

--- jobs/freifunk.rb
+++ jobs/freifunk.rb
@@ -2,7 +2,7 @@ require("uri")
 require("net/http")
 require("json")
 
-uri = URI('https://map.freifunk-duesseldorf.de/alfred_merged.json')
+uri = URI('https://map.freifunk-duesseldorf.de/data/nodes.json')
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
 request = Net::HTTP::Get.new(uri.request_uri)
@@ -11,7 +11,7 @@ request = Net::HTTP::Get.new(uri.request_uri)
 SCHEDULER.every '1m', :first_in => 0 do |job|
   begin
     json = JSON.parse(http.request(request).body)
-    value = json.length()
+    value = json["nodes"].length()
     send_event('freifunk-total', { current: value, status: "normal" })
   rescue
     send_event('freifunk-total', { current: 0, status: "warning" })
