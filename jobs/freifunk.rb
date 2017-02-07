b06780291b68f4d87877136b59689c0d23ae3646
Niklas Sombert <niklas@ytvwld.de>
2017-02-07 13:06:15 +0100

    Correctly parse the JSON we get from Freifunk.

--- jobs/freifunk.rb
+++ jobs/freifunk.rb
@@ -1,5 +1,6 @@
 require("uri")
 require("net/http")
+require("json")
 
 uri = URI('https://map.freifunk-duesseldorf.de/alfred_merged.json')
 http = Net::HTTP.new(uri.host, uri.port)
@@ -9,8 +10,8 @@ request = Net::HTTP::Get.new(uri.request_uri)
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
   begin
-    json = http.request(request).body
-    value = json.split("\n").grep(/node_id/).length()
+    json = JSON.parse(http.request(request).body)
+    value = json.length()
     send_event('freifunk-total', { current: value, status: "normal" })
   rescue
     send_event('freifunk-total', { current: 0, status: "warning" })
