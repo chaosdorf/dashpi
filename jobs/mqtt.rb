f8f37798f6da2483fbe1f33250a84ff4430d8ef2
Niklas Sombert <niklas@ytvwld.de>
2017-08-18 21:49:44 +0200

    Get the music from mqtt.

--- jobs/mqtt.rb
+++ jobs/mqtt.rb
@@ -1,11 +1,14 @@
 require 'mqtt'
 
 power_series = Array.new(20).fill(0)
+music_status = {}
+last_music_update = Time.now
 
 SCHEDULER.every '5m', :allow_overlapping => false, :first_in => 0 do |job|
   MQTT::Client.connect('mqttserver') do |client|
     client.subscribe('sensors/flukso/power/sum/30s_average') # power
     client.subscribe('door/status') # door
+    client.subscribe('music/#') # music
 
     client.get do |topic,message|
       if topic == 'sensors/flukso/power/sum/30s_average'
@@ -30,6 +33,17 @@ SCHEDULER.every '5m', :allow_overlapping => false, :first_in => 0 do |job|
         # D O O R                        #
         ##################################
         send_event('chaosdoor-mode', { text: message } )
+      elsif topic.start_with?('music/')
+        ##################################
+        # M U S I C                      #
+        ##################################
+        music_status[topic.split('/')[1]] = message
+        if last_music_update < (Time.now - 5)
+          last_music_update = Time.now
+          SCHEDULER.in '1s' do
+            send_event('music', music_status.clone)
+          end
+        end
       end
     end
   end
