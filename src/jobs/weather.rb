2ae1d76349abf33b100dada0a85c670b2a7d88ec
Niklas Sombert <niklas@ytvwld.de>
2022-04-03 21:52:24 +0200

    Migrate weather to new JSON API

--- src/jobs/weather.rb
+++ src/jobs/weather.rb
@@ -1,34 +1,17 @@
 # encoding: utf-8
 
 require 'net/http'
-require 'xmlsimple'
-require 'time'
-require 'date'
+require 'json'
+
+API = URI.parse('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.22172&lon=6.77616&altitude=45')
 
 SCHEDULER.every '1h', first_in: 0 do |_job|
-    response = Net::HTTP.get('www.yr.no', '/place/Germany/North_Rhine-Westphalia/Düsseldorf/forecast_hour_by_hour.xml')
-    xml = XmlSimple.xml_in(response)
-    location = xml['location'][0]['name'][0]
-    sun = xml['sun'][0]
-    data = xml['forecast'][0]['tabular'][0]['time']
-    now = { condition: data[0]['symbol'][0]['name'],
-            symbol: climacon_class(data[0]['symbol'][0]['name'],
-                                   is_sunlight(data[0]['from'],
-                                               data[0]['to'],
-                                               sun)),
-            temp: "#{data[0]['temperature'][0]['value']}°C" }
-    onehour = { condition: data[1]['symbol'][0]['name'],
-                symbol: climacon_class(data[1]['symbol'][0]['name'],
-                                       is_sunlight(data[1]['from'],
-                                                   data[1]['to'],
-                                                   sun)),
-                temp: "#{data[1]['temperature'][0]['value']}°C" }
-    twohour = { condition: data[2]['symbol'][0]['name'],
-                symbol: climacon_class(data[2]['symbol'][0]['name'],
-                                       is_sunlight(data[2]['from'],
-                                                   data[2]['to'],
-                                                   sun)),
-                temp: "#{data[1]['temperature'][0]['value']}°C" }
+    response = Net::HTTP.get(API, {'User-Agent' => 'chaosdorf dashboard'})
+    json = JSON.parse(response)
+    data = json['properties']['timeseries']
+    now = parse_timeseries(data[0])
+    onehour = parse_timeseries(data[1])
+    twohour = parse_timeseries(data[2])
     send_event('weather',
                title: 'Weather',
                now: now,
@@ -36,29 +19,38 @@ SCHEDULER.every '1h', first_in: 0 do |_job|
                twohour: twohour)
 end
 
-def is_sunlight(from, to, sun)
-    risehour = Time.parse(sun['rise']).hour
-    sethour = Time.parse(sun['set']).hour
-    fromhour = Time.parse(from).hour
-    tohour = Time.parse(to).hour
-    # close enough
-    (fromhour.between?(risehour, sethour) && tohour.between?(risehour, sethour))
+def parse_timeseries(ts)
+    symbol = ts['data']['next_1_hours']['summary']['symbol_code']
+    {
+        condition: symbol.split('_')[0],
+        symbol: climacon_class(symbol),
+        temp: "#{ts['data']['instant']['details']['air_temperature']}°C",
+    }
 end
 
-def climacon_class(weather_code, sunlight)
+def climacon_class(weather_code)
     # see https://github.com/christiannaths/Climacons-Font/blob/master/webfont/climacons-font.css
     # see https://github.com/AdamWhitcroft/Climacons/tree/master/SVG
-    return 'drizzle' if weather_code == 'Light rain'
-    return 'drizzle sun' if (weather_code == 'Light rain showers') && sunlight
-    return 'drizzle moon' if (weather_code == 'Light rain showers') && !sunlight
-    return 'rain' if weather_code == 'Rain'
-    return 'rain sun' if (weather_code == 'Rain showers') && sunlight
-    return 'rain moon' if (weather_code == 'Rain showers') && !sunlight
-    return 'cloud' if weather_code == 'Cloudy'
-    return 'cloud sun' if (weather_code == 'Partly cloudy') && sunlight
-    return 'cloud moon' if (weather_code == 'Partly cloudy') && !sunlight
-    return 'sun' if (weather_code == 'Clear sky') && sunlight
-    return 'sun' if (weather_code == 'Fair') && sunlight
-    return 'moon' if (weather_code == 'Fair') && !sunlight
-    return 'moon' if (weather_code == 'Clear sky') && !sunlight
+    return 'drizzle' if weather_code == 'lightrain'
+    return 'showers sun' if weather_code == 'lightrainshowers_day'
+    return 'showers moon' if weather_code == 'lightrainshowers_night'
+    return 'rain' if weather_code == 'rain'
+    return 'showers sun' if weather_code == 'rainshowers_day'
+    return 'showers moon' if weather_code == 'rainshowers_night'
+    return 'downpour' if weather_code == 'heavyrain'
+    return 'downpour sun' if weather_code == 'heavyrainshowers_day'
+    return 'downpour moon' if weather_code == 'heavyrainshowers_night'
+    return 'sleet' if weather_code == 'sleet'
+    return 'sleet' if weather_code == 'heavysleet' # TODO?
+    return 'snow' if weather_code == 'lightsnow' # TODO?
+    return 'snow' if weather_code == 'snow'
+    return 'snow sun' if weather_code == 'lightsnowshowers_day'
+    return 'snow moon' if weather_code == 'lightsnowshowers_night'
+    return 'cloud' if weather_code == 'cloudy'
+    return 'cloud sun' if weather_code == 'partlycloudy_day'
+    return 'cloud moon' if weather_code == 'partlycloudy_night'
+    return 'sun' if weather_code == 'fair_day'
+    return 'moon' if weather_code == 'fair_night'
+    return 'sun' if weather_code == 'clearsky_day'
+    return 'moon' if weather_code == 'clearsky_night'
 end
